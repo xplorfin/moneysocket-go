d29efb6dd5cbe265689c64d5aab14593e568ae90
Jake Sylvestre <jakesyl@gmail.com>
2021-03-07 05:01:00 -0500

    fix cipher text issue

--- moneysocket/ws/ws_server/client.go
+++ moneysocket/ws/ws_server/client.go
@@ -101,12 +101,6 @@ func (c *Client) writePump() error {
 			}
 			w.Write(message)
 
-			// Add queued chat messages to the current websocket message.
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-c.send)
-			}
 
 			if err := w.Close(); err != nil {
 				return err
