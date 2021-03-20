797abd9052a43646086f3efb9f9142cf48e6a76e
Jake Sylvestre <jakesyl@gmail.com>
2021-03-19 19:59:04 -0700

    add lnd handler

--- moneysocket/lightning/lightning.go
+++ moneysocket/lightning/lightning.go
@@ -4,14 +4,16 @@
 package lightning
 
 // TODO this is a callback
-type PaidCallback func()
+type PaidCallback func(preimage string, msats int)
 
 // Lightning is an implementable interface for interacting with different lightning clients
 type Lightning interface {
 	// RegisterPaidRecvCb registers a PaidCallback for when an invoice is paid
 	RegisterPaidRecvCb(callback PaidCallback)
 	// GetInvoice retrieves a bolt11 invoice for a given msatAmount
-	GetInvoice(msatAmount int)
+	GetInvoice(msatAmount int) (paymentRequest string, err error)
 	// PayInvoice pays a bolt11 invoice
-	PayInvoice(bolt11 string)
+	PayInvoice(bolt11 string) (preimage []byte, msatAmount int, err error)
+	// RecvPaid receives a payment
+	RecvPaid(preimage string, msats int)
 }
--- moneysocket/lightning/lnd.go
+++ moneysocket/lightning/lnd.go
@@ -1,8 +1,11 @@
 package lightning
 
 import (
+	"context"
+	"log"
+
+	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/xplorfin/moneysocket-go/moneysocket/config"
-	"google.golang.org/grpc"
 )
 
 // Lnd is the Lightning interface for lnd
@@ -11,34 +14,75 @@ type Lnd struct {
 	config *config.Config
 	// paidRecvCb is the callback for a paid invoice receipt
 	paidRecvCb PaidCallback
-	// dialContext is the grpc connection for
-	dialContext *grpc.ClientConn
+	// client is the lnd client
+	client lnrpc.LightningClient
+	// pendingPaymentHashes contains a list of pending payments
+	pendingPaymentHashes [][]byte
+}
+
+// RecvPaid receives an invoice payment
+func (l *Lnd) RecvPaid(preimage string, msats int) {
+	l.paidRecvCb(preimage, msats)
 }
 
 // NewLnd generates a new lnd client from a given config
-func NewLnd(config *config.Config) *Lnd {
+// TODO use streaming for invoices
+func NewLnd(config *config.Config) (lnd *Lnd, err error) {
+	client, err := config.LndConfig.RPCClient(context.Background())
+	if err != nil {
+		return lnd, err
+	}
+
 	return &Lnd{
 		config:     config,
 		paidRecvCb: nil,
+		client:     client,
+	}, nil
+}
+
+// RegisterPaidRecvCb registers the callback
+func (l *Lnd) RegisterPaidRecvCb(callback PaidCallback) {
+	l.paidRecvCb = callback
+}
+
+// GetInvoice gets an invoice (paymentRequest) fora given amount msatAmount
+func (l *Lnd) GetInvoice(msatAmount int) (paymentRequest string, err error) {
+	log.Printf("getting invoice %d msats", msatAmount)
+	satAmount := int64(msatAmount / 1000.0)
+
+	invoice := &lnrpc.Invoice{
+		Memo:      "",
+		Value:     satAmount,
+		ValueMsat: satAmount,
+		Expiry:    3600,
+		Private:   false,
 	}
+
+	resp, err := l.client.AddInvoice(context.Background(), invoice)
+	if err != nil {
+		return paymentRequest, err
+	}
+	l.pendingPaymentHashes = append(l.pendingPaymentHashes, resp.RHash)
+	return resp.PaymentRequest, err
 }
 
-// TODO
-func getGrpcConnection(config *config.Config) (conn *grpc.ClientConn, err error) {
-	//cert, err := config.RpcConfig.BindPort
-	return nil, err
-}
+// PayInvoice sends a payment for a bolt-11 invoice
+func (l *Lnd) PayInvoice(bolt11 string) (preimage []byte, msatAmount int, err error) {
+	log.Printf("paying invoice %s", bolt11)
+	// TODO figure out deprecation status hre
+	payReq := lnrpc.SendRequest{
+		PaymentRequest: bolt11,
+	}
+	resp, err := l.client.SendPaymentSync(context.Background(), &payReq)
+	if err != nil {
+		return preimage, msatAmount, err
+	}
 
-func (l Lnd) RegisterPaidRecvCb(callback PaidCallback) {
-	panic("implement me")
-}
+	log.Println(resp)
+	log.Printf("paid %s", payReq.PaymentRequest)
+	log.Printf("route %s", resp.PaymentRoute)
+	return resp.PaymentPreimage, int(resp.PaymentRoute.TotalAmtMsat), nil
 
-func (l Lnd) GetInvoice(msatAmount int) {
-	panic("implement me")
-}
-
-func (l Lnd) PayInvoice(bolt11 string) {
-	panic("implement me")
 }
 
 var _ Lightning = &Lnd{}
